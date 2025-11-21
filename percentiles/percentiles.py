53bae84947de25a17575b828e0207aea5d969ae2
Vladimir Seregin <31631@rambler.ru>
2025-11-21 11:35:11 +0400

    modernize (#12)
    * pyproject.toml! 
    * uv! 
    * ruff!
    * github actions!

--- percentiles/percentiles.py
+++ percentiles/percentiles.py
@@ -1,28 +1,29 @@
 import math
+from decimal import Decimal
 from typing import Iterable
 
 
-def percentile(N: Iterable, percent: int):
+def percentile(numbers: Iterable[int | float | Decimal], percent: int) -> float:
     """
     Find the percentile of a list of values.
     Stolen from http://code.activestate.com/recipes/511478-finding-the-percentile-of-the-values/
     """
-    if not N:
-        raise ValueError('N must be non-empty iterable')
+    if not numbers:
+        raise ValueError("N must be non-empty iterable")
 
-    if not (0 < percent < 100 and type(percent) == int):
-        raise ValueError('percent parameter must be integer from 0 to 100')
+    if not (0 < percent < 100 and isinstance(percent, int)):
+        raise ValueError("percent parameter must be integer from 0 to 100")
 
-    N = sorted(N)
+    numbers = sorted(numbers)
 
-    k = (len(N) - 1) * percent / 100
+    k = (len(numbers) - 1) * percent / 100
     prev_index = math.floor(k)
     next_index = math.ceil(k)
 
     if prev_index == next_index:
-        return N[int(k)]
+        return float(numbers[int(k)])
 
-    d0 = N[prev_index] * (next_index - k)
-    d1 = N[next_index] * (k - prev_index)
+    d0 = float(numbers[prev_index]) * (next_index - k)
+    d1 = float(numbers[next_index]) * (k - prev_index)
 
     return d0 + d1
